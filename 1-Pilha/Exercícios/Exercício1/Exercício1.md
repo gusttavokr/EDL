08ecd877e1afeca3671479da5d41000e29671769
Gustavo Maia Militão <20241014040006@ifrn.local>
2025-04-15 11:48:01 -0300

    :wrench: Config: Configurando repositório de EDL

---
+++ 1-Pilha/Exercícios/Exercício1/Exercício1.md
@@ -0,0 +1,11 @@
+1) Refazer depois
+
+2)
+|     | Incremento 10 | Incremento 100 | Incremento 1000 | duplicação |
+|----------|----------|----------|----------|----------|
+| 10 elementos  | Linha 1  | Linha 1  | Linha 1  | Linha 1  |
+| 100 elementos  | Linha 2  | Linha 2  | Linha 2  | Linha 2  |
+| 1000 elementos  | Linha 3  | Linha 3  | Linha 3  | Linha 3  |
+| 10000 elementos  | Linha 4  | Linha 4  | Linha 4  | Linha 4  |
+| 100000 elementos  | Linha 5  | Linha 5  | Linha 5  | Linha 5  |
+| 1000000 elementos  | Linha 6  | Linha 6  | Linha 6  | Linha 6  |
---
+++ 1-Pilha/Exercícios/Exercício1/PilhaArray.java
@@ -0,0 +1,56 @@
+public class PilhaArray implements Pilha{
+    private int capacidade;
+    private Object[] a; // O array
+    private int t; // O topo
+    private int FC; // fator de crescimento (??)
+
+    public PilhaArray (int capacidade, int crescimento){
+        this.capacidade = capacidade;
+        t =- 1; // O topo sempre será o ultimo
+        FC = crescimento;
+        if (crescimento<=0){
+            FC = 0;
+        }
+        a = new Object[capacidade];
+    }
+
+    public void push(Object o){
+        if (t>= capacidade -1){ // Se o topo for maior ou igual a capacidade
+            if (FC==0){ // Se o fator de crescimento for igual a zero
+                capacidade*=2; // Dobre a capacidade
+            }
+            else{
+                capacidade += FC; // Incremente a capacidade com o fator de crescimento
+            }
+            Object b[] = new Object[capacidade]; // Cria o array secundario com base na nova capacidade
+            for (int f=0; f<a.length; f++){
+                b[f] = a[f]; // Passando os elementos para o novo array
+            }
+            a = b;
+        }
+        a[++t] = o; // O topo agora é o novo elemento
+    }
+
+    public Object pop() throws PilhaVaziaExcecao {
+        if (isEmpty()){ // Verifica se a pilha está vazia
+            throw new PilhaVaziaExcecao("A pilha está vazia!");
+        }
+        Object r = a[t--]; // Remove o elemento
+        return r; // Retorna o elemento
+    }
+
+    public boolean isEmpty(){
+        return t==-1; // Retorna um bool se tá vazio ou não se o topo for -1
+    }
+
+    public int size(){
+        return t+1; // Retorna o tamanho do array
+    }
+
+    public Object top() throws PilhaVaziaExcecao{
+        if (isEmpty()){
+            throw new PilhaVaziaExcecao("A pilha está vazia");
+        }
+        return a[t];
+    }
+}
