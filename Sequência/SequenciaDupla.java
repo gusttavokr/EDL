43be235a414c76d3a0155e522bf2d1fc67cbf358
Gustavo Maia <gustavommilitao06@gmail.com>
2025-05-26 00:50:23 +0000

    :heavy_check_mark: test: Implementando Sequência com Lista Duplamente Encadeada

--- Sequência/SequenciaDupla.java
+++ Sequência/SequenciaDupla.java
@@ -107,14 +107,111 @@ public class SequenciaDupla implements Sequencia{
 
     public int rankOf(Node n){
         validacao();
+
+        Node temp = NoInicio.getProximo();
+
+        for (int i = 0; temp != NoFim; i++){
+            if (temp == n) {
+                return i;
+            } 
+            temp = temp.getProximo();
+        }
+
+        throw new SequenciaInvalida("Esse nó não existe!"); 
     }
 
-    public void validacao(){
+    public Node first(){
+        validacao();
+        return NoInicio.getProximo();
+    }
+
+    public Node last(){
+        validacao();
+        return NoFim.getAnterior();
+    }
+
+    public void replaceElement(Node n, Object o){
+        ObjectNoNull(n);
+        n.setElemento(o);
+    }
+
+    public void swapElements(Node n, Node q){
+        ObjectNoNull(n);
+        ObjectNoNull(q);
+        
+        Object antigo = n.getElemento();
+        n.setElemento(q.getElemento());
+        q.setElemento(antigo);
+    }
+
+    public void insertBefore(Node n, Object o){
+        ObjectNoNull(n);
+
+        Node novo = new Node(o);
+        novo.setAnterior(n.getAnterior());
+        novo.setProximo(n);
+
+        n.getAnterior().setProximo(novo);
+        n.setAnterior(novo);   
+        tamanho++;    
+    }
+
+    public void insertAfter(Node n, Object o){
+        ObjectNoNull(n);
+
+        Node novo = new Node(o);
+
+        novo.setProximo(n.getProximo());
+        novo.setAnterior(n);
+        n.getProximo().setAnterior(novo);
+        n.setProximo(novo);
+
+        tamanho++;
+    }
+
+    public void insertFirst(Node n, Object o){
+        ObjectNoNull(n);
+
+        Node novo = new Node(o);
+        novo.setAnterior(NoInicio);
+        novo.setProximo(NoInicio.getProximo());
+        NoInicio.getProximo().setAnterior(novo);
+        NoInicio.setProximo(novo);
+        tamanho++;
+    }
+
+    public void insertLast(Node n, Object o){
+        ObjectNoNull(n);
+
+        Node novo = new Node(o);
+
+        novo.setProximo(NoFim);
+        novo.setAnterior(NoFim.getAnterior());
+        NoFim.getAnterior().setProximo(novo);
+        NoFim.setAnterior(novo);
+        tamanho++;
+    }
+
+    public void remove(Node n){
+        ObjectNoNull(n);
+
+        n.getAnterior().setProximo(n.getProximo());
+        n.getProximo().setAnterior(n.getAnterior());
+        tamanho--;
+    }
+
+    public void validacao(int r){
         if (isEmpty()) {
             throw new SequenciaInvalida("A lista está vazia");
         }
         if (r < 0 || r > tamanho) {
             throw new SequenciaInvalida("Rank inválido!"); 
+        }
+    }
+
+    public void ObjectNoNull(Node n){
+        if (n == null) {
+            throw new SequenciaInvalida("Nó não pode ser null!");
         }
     }
 }
